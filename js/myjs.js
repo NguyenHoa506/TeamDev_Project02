04c68bbe81721c4a2e0980683275e8a86f7f09a7
Lam <51301989@hcmut.edu.vn>
2017-06-29 11:43:27 +0700

    gop jquery

--- js/myjs.js
+++ js/myjs.js
@@ -1,9 +1,36 @@
 $(document).ready(function(){
-    $(".button_sp").click(function(){
-        $("#section1").hide();
-        $("#section3").hide();
-    });
-    $(".button_sp").click(function(){
-        $("#section2").show();
-    });
-});
+		        $(".button_sp1").click(function(){
+			        $("#section2").hide();
+			        $("#section3").hide();
+			        $("#section1").show();
+		        });
+		        $(".button_sp").click(function(){
+			    
+			    });
+		    }); 
+		   
+
+		    
+		    $(document).ready(function(){ 
+		    	$(".button_sp3").click(function(){
+                    $("#section1").hide();
+                    $("#section2").hide();
+			        $("#section3").show();
+			    });
+			    $(".button_sp").click(function(){
+			        
+			    });
+			});
+        
+
+        
+        	$(document).ready(function(){		 
+			    $(".button_sp2").click(function(){
+			    	$("#section1").hide();
+			    	$("#section3").hide();
+			    	$("#section2").show();
+			    });
+			    $(".button_sp").click(function(){
+
+			    });
+			    });

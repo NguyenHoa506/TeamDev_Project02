b2b00caed855f77da281d6a9bb495692725362ec
huongnguyen071723 <huongnguyen071723@gmail.com>
2017-06-29 09:56:52 +0700

    update index and css

--- js/myjs.js
+++ js/myjs.js
@@ -1,33 +1,33 @@
-$(document).ready(function(){
+$(function(){
 	$(".button_sp1").click(function(){
 		$("#section2").hide();
 		$("#section3").hide();
 		$("#section1").show();
 	});
-		$(".button_sp").click(function(){
+		$(".button_sp1").click(function(){
 	});
 }); 
 
 
 
-$(document).ready(function(){ 
+$(function(){ 
     $(".button_sp3").click(function(){
         $("#section1").hide();
         $("#section2").hide();
         $("#section3").show();
     });
-    $(".button_sp").click(function(){
+    $(".button_sp3").click(function(){
     });
 });
 
 
 
-$(document).ready(function(){         
+$(function(){         
     $(".button_sp2").click(function(){
         $("#section1").hide();
         $("#section3").hide();
         $("#section2").show();
     });
-        $(".button_sp").click(function(){
+        $(".button_sp2").click(function(){
         });
 });
